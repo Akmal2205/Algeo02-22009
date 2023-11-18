9e9def491f3c6e2d7f443f591385390b8a4f1b3b
cupski <yusufrafi77@gmail.com>
2023-11-18 11:43:12 +0700

    create: multiple files request

--- source/Backend/project/api/urls.py
+++ source/Backend/project/api/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import ImageUploadView
+from .views import ImageUploadView, MultipleFileUploadView
+from rest_framework.routers import DefaultRouter
+
+
+# router = DefaultRouter()
+# router.register('folders', FolderViewSet, basename='folders')
 
 urlpatterns = [
-    path('api/upload/', ImageUploadView.as_view(), name='image-upload'),
-    # ... other endpoints
+    path('api/upload_image/', ImageUploadView.as_view(), name='image-upload'),
+    path('api/upload_folder/', MultipleFileUploadView.as_view(), name='folder-upload'),
+    # path('', include(router.urls))
 ]
                                  
