d2b4c8ec3b2ee1e1a581900620155c887f098016
cupski <yusufrafi77@gmail.com>
2023-11-19 19:28:17 +0700

    Final product

--- source/FEALGEO/src/Pages/HowPage.jsx
+++ source/FEALGEO/src/Pages/HowPage.jsx
@@ -3,6 +3,12 @@ import "./How.css"
 export const HowPage = () => {
   return (
     <div className="how-container">
+      <div className="instruction-container">
+        <h3 className="teks">How to use :</h3>
+        <p className="teks">1. Masukkan gambar referensi dengan menekan "Insert an Image", lalu tekan "Upload" untuk mengunggah.</p>
+        <p className="teks">2. Geser layar ke bawah dan tekan "Insert dataset" untuk memasukkan dataset, lalu tekan "Upload" untuk mengunggah.</p>
+        <p className="teks">3. Geser layar ke atas lagi, lalu tekan tombol search untuk memulai proses pencarian.</p>
+      </div>
     </div>
   )
 }
